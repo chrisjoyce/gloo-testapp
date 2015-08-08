2f2411a85a55336e455cd872b9d0761001bf710f
Chris Joyce <ctjoyce@gmail.com>
2015-08-08 17:27:29 -0600

    Broke out new/update from normal contrller.

--- source/client/app/books/book.controller.js
+++ source/client/app/books/book.controller.js
@@ -6,19 +6,33 @@
 		.controller('BookController', BookController);
 
 
-	BookController.$inject = ['BookService'];
+	BookController.$inject = ['BookService', '$routeParams'];
 
-	function BookController(BookService) {
+	function BookController(BookService, $routeParams) {
 		var vm = this;
 
 		vm.title = 'Books!';
 		vm.books = [];
-
+		vm.newBook = false;
+		vm.deleteBook = deleteBook;
 
 		activateController();
 
 		function activateController() {
 			vm.books = BookService.getAll();
+		}
+
+		function deleteBook(id) {
+			BookService.delete(id).then(DeleteSuccessCB, DeleteFailCB);
+
+			function DeleteSuccessCB() {
+
+			}
+
+			function DeleteFailCB() {
+
+			}
+
 		}
 	}
 
--- source/client/app/books/book.service.js
+++ source/client/app/books/book.service.js
@@ -4,16 +4,22 @@
 		.factory('BookService', BookService);
 
 
-	BookService.$inject = ['$http'];
+	BookService.$inject = ['$http', '$q'];
 
 
-	function BookService($http) {
+	function BookService($http, $q) {
+
+		var books = [
+				{title: 'Design Patterns', author: 'Many People', ISBN: '123AI-34343-BN33'},
+				{title: 'Javascript Cooking', author: 'Douglas Crockford', ISBN: '123AI-34343-BN34'}
+	    ];
 
 		var service = {
 			getAll: getBooks,
 			get: getBook,
 			delete: deleteBook,
-			update: updateBook
+			update: updateBook,
+			create: createBook
 		};
 
 
@@ -22,23 +28,46 @@
 
 		// service api, using promises
 		function getBooks() {
-			return [
-				{title: 'Design Patterns', author: 'Many People', ISBN: '123AI-34343-BN33'},
-				{title: 'Javascript Cooking', author: 'Douglas Crockford', ISBN: '123AI-34343-BN34'}
-	    ];
+			return books;
 		}
 
 		function getBook(title) {
-			console.log("TODO getBook")
+			var book = books.filter(function(book) {
+				return book.title === title;
+			});
+
+			// so I know this is a bit hacky, but I'm assuming unique titles atm
+			return $q.when(book[0]);
 		}
 
 		function deleteBook(id) {
-			console.log("TODO delete");
+			books.splice(idx, 1);
+			return $q.when(books);
 
+
+			for (var idx = 0; idx < books.length; idx++) {
+				var book = books[idx];
+				if (book.title === id) {
+					debugger;
+					books.splice(idx, 1);
+					break;
+				}
+			}
+
+			return $q.when(books);
 		}
 
 		function updateBook(id) {
 			console.log("TODO update");
+		}
+
+		function createBook(book) {
+			// assume valid stuff for now
+			books.push(book);
+			// simulate a network call if i don't get to writing a server
+			return $q.when(books);
+			// or
+			//return $http.post('/api/books/new', book);
 		}
 	}
 
